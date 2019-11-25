38ad8b6f7a6ac4b66c0000eedda2931ced53b594
Damir15 <krosava_15d@mail.ru>
2019-11-25 23:49:17 +0600

    Changed struck project

--- api/cmd/server/server.go
+++ api/cmd/server/server.go
@@ -1,10 +1,20 @@
 package server
 
 import (
+	"context"
 	"github.com/koind/banner-rotation/api/internal/config"
-	"github.com/koind/banner-rotation/api/internal/transport"
+	"github.com/koind/banner-rotation/api/internal/db"
+	"github.com/koind/banner-rotation/api/internal/domain/service"
+	"github.com/koind/banner-rotation/api/internal/rabbit"
+	"github.com/koind/banner-rotation/api/internal/storage/postgres"
+	"github.com/koind/banner-rotation/api/internal/transport/grpc"
+	"github.com/koind/banner-rotation/api/internal/transport/http"
 	"github.com/spf13/cobra"
+	"github.com/streadway/amqp"
+	"go.uber.org/zap"
+	"log"
 	"os"
+	"time"
 )
 
 // Declaring commands to start server
@@ -13,11 +23,61 @@ var RunServerCmd = &cobra.Command{
 	Short: "Run server",
 	Run: func(cmd *cobra.Command, args []string) {
 		cfg := config.Init(config.Path)
-		s := transport.NewServer(cfg)
+		rotationService, publisher, logger := Init(cfg)
 		serverType := os.Getenv("SERVER_TYPE")
 
-		s.Run(serverType)
+		switch serverType {
+		case "HTTP":
+			httpRotationService := http.NewHTTPRotationService(*rotationService, publisher, logger)
+			hs := http.NewHTTPServer(httpRotationService, cfg.HTTPServer.GetDomain())
+
+			logger.Error("Error starting http server", zap.Error(hs.Start()))
+		case "GRPC":
+			gs := grpc.NewGRPCServer(cfg.GRPCServer.GetDomain(), *rotationService, publisher, logger)
+
+			logger.Error("Error starting grpc server", zap.Error(gs.Start()))
+		default:
+			log.Fatal("Specified the wrong server type")
+		}
 	},
+}
+
+// Returns the initialized objects needed to start the server
+func Init(cfg config.Options) (*service.RotationService, *rabbit.Publisher, *zap.Logger) {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Sync()
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		time.Duration(cfg.Postgres.PingTimeout)*time.Millisecond,
+	)
+	defer cancel()
+
+	pg, err := db.IntPostgres(ctx, config.Postgres(cfg.Postgres))
+	if err != nil {
+		log.Fatalf("failing to connect to the database %v", err)
+	}
+
+	conn, err := amqp.Dial(cfg.RabbitMQ.URL)
+	if err != nil {
+		log.Fatalf("failing to connect to the rabbitmq %v", err)
+	}
+	defer conn.Close()
+
+	rotationRepository := postgres.NewRotationRepository(pg, *logger)
+	statisticRepository := postgres.NewStatisticRepository(pg, *logger)
+	statisticService := service.StatisticService{StatisticRepository: statisticRepository}
+	publisher := rabbit.NewPublisher(*conn, cfg.RabbitMQ.ExchangeName, cfg.RabbitMQ.QueueName)
+	rotationService := service.RotationService{
+		StatisticService:    &statisticService,
+		RotationRepository:  rotationRepository,
+		StatisticRepository: statisticRepository,
+	}
+
+	return &rotationService, publisher, logger
 }
 
 // When initializing parse the path to the configuration
